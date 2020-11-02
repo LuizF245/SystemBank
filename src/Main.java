3bfd85c1ca1a90c687cb690229dbca97ea2ee5b4
LuizF245 <fernandoalmeidaluiz769@gmail.com>
2020-11-02 14:37:45 -0300

    Created a new "bank account"

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        ContaBanco contaBanco = new ContaBanco();
+        ContaBanco p1 = new ContaBanco();
+        ContaBanco p2 = new ContaBanco();
 
-        contaBanco.abrirConta("cp", "Luiz Fernando");
-        contaBanco.sacar(150.0f);
-        contaBanco.depositar(500.0f);
-        contaBanco.pagarMensal();
+        p1.abrirConta("cp", "Luiz Fernando", 123);
+        p1.sacar(150.0f);
+        p1.depositar(500.0f);
+        p1.pagarMensal();
+        p1.estadoAtual();
 
-        contaBanco.estadoAtual();
+        p2.abrirConta("cc", "Gabriel Vinhais", 321);
+        p2.depositar(1800.0f);
+        p2.sacar(6000.f);
+        p2.pagarMensal();
+        p2.estadoAtual();
     }
 
 }
