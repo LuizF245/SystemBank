e31011baa7b684fa432384c23650f68aae74270e
LuizF245 <fernandoalmeidaluiz769@gmail.com>
2020-11-02 14:24:42 -0300

    Added new method and optimized the code

--- src/ContaBanco.java
+++ src/ContaBanco.java
@@ -1,61 +1,61 @@
-import java.util.Scanner;
-
 public class ContaBanco {
 
+    // Atributos
     private int numConta;
     private String tipo;
     private String dono;
     private float saldo;
     private boolean status;
 
-    public ContaBanco() {
-        saldo = 0;
-        status = false;
+    // Métodos Personalizados
+    public void estadoAtual() {
+        System.out.println("Conta: " + this.getNumConta());
+        System.out.println("Tipo: " + this.getTipo());
+        System.out.println("Dono: " + this.getDono());
+        System.out.println("Saldo: R$" + this.getSaldo());
+        System.out.println("Status: " + this.isStatus());
     }
 
     public void abrirConta(String tipo, String dono) {
-        setDono(dono);
-        setTipo(tipo);
-        setNumConta(getNumConta() + 1);
-        setStatus(true);
+        this.setDono(dono);
+        this.setTipo(tipo);
+        this.setNumConta(getNumConta() + 1);
+        this.setStatus(true);
 
-        System.out.println("Seu nome é " + getDono());
-        System.out.println("Você está abrindo uma " + getTipo());
-        System.out.println("O número da sua conta é " + getNumConta());
-
-        if (getTipo().equalsIgnoreCase("Conta Poupança")) {
-            setSaldo(getSaldo() + 150);
-            System.out.println("Você optou por abrir uma " + getTipo() + ". E por isso ganhou R$" + getSaldo() + " em sua conta!");
-        } else if (getTipo().equals("Conta Corrente")) {
-            setSaldo(getSaldo() + 50);
-            System.out.println("Você optou por abrir uma " + getTipo() + ". E por isso ganhou R$" + getSaldo() + " em sua conta!");
+        if (this.getTipo().equalsIgnoreCase("cc")) {
+            this.setSaldo(50);
+            System.out.println("Você abriu uma conta corrente, por isso ganhou R$50,00.");
+        } else if (this.getTipo().equalsIgnoreCase("cp")) {
+            this.setSaldo(150);
+            System.out.println("Você abriu uma conta poupança, por isso ganhou R$150,00.");
         }
 
         mostrarSaldo();
-        System.out.println("Sua conta se encontra " + isStatus());
+        System.out.println("Conta aberta com Sucesso!");
+        System.out.println("Sua conta se encontra " + this.isStatus());
         pularLinha();
     }
 
-    void fecharConta() {
+    public void fecharConta() {
 
-        if (getSaldo() < 0) {
+        if (this.getSaldo() < 0) {
             System.out.println("Você não consegue fechar a sua conta, pois você está como devedor em nosso banco!");
-        } else if (getSaldo() > 0) {
+        } else if (this.getSaldo() > 0) {
             System.out.println("Você precisa sacar todo seu dinheiro para fechar a conta!");
         } else {
-            setStatus(false);
+            this.setStatus(false);
             System.out.println("Você fechou sua conta com sucesso!");
-            System.out.println("Sua conta se encontra " + isStatus());
+            System.out.println("Sua conta se encontra " + this.isStatus());
         }
 
         pularLinha();
     }
 
-    void depositar(float valorDeposito) {
-        if (status == false) {
+    public void depositar(float valorDeposito) {
+        if (!this.isStatus()) {
             System.out.println("Você não consegue depositar dinheiro sem ter uma conta!");
         } else {
-            setSaldo(getSaldo() + valorDeposito);
+            this.setSaldo(this.getSaldo() + valorDeposito);
             System.out.println("Você depositou em sua conta R$" + valorDeposito);
             mostrarSaldo();
         }
@@ -63,22 +63,22 @@ public class ContaBanco {
         pularLinha();
     }
 
-    void sacar(float valorSaque) {
+    public void sacar(float valorSaque) {
 
-        if (isStatus() == false) {
+        if (!this.isStatus()) {
             System.out.println("Você não pode sacar dinheiro sem ter uma conta!");
         } else {
-            if (getSaldo() == 0) {
+            if (this.getSaldo() == 0) {
                 System.out.println("Você não possui dinheiro suficiente para sacar.");
-            } else if (getTipo().equalsIgnoreCase("Conta Corrente")) {
-                    setSaldo(getSaldo() - valorSaque);
+            } else if (this.getTipo().equalsIgnoreCase("cc")) {
+                    this.setSaldo(this.getSaldo() - valorSaque);
                     System.out.println("Você sacou R$" + valorSaque);
-                    System.out.println("E ficou com devedor de R$" + getSaldo() + " para o Banco.");
-            } else if (getSaldo() < valorSaque) {
-                System.out.println("Você não pode ficar como devedor no Banco pois a sua conta é " + getTipo());
+                    System.out.println("E ficou com devedor de R$" + this.getSaldo() + " para o Banco.");
+            } else if (this.getSaldo() < valorSaque) {
+                System.out.println("Você não pode ficar como devedor no Banco pois a sua conta é " + this.getTipo());
                 mostrarSaldo();
             } else {
-                setSaldo(getSaldo() - valorSaque);
+                this.setSaldo(this.getSaldo() - valorSaque);
                 System.out.println("Você sacou R$" + valorSaque);
                 mostrarSaldo();
             }
@@ -89,30 +89,39 @@ public class ContaBanco {
 
     public void pagarMensal() {
 
-        float valorMensalidade = 0;
+        int valorMensalidade = 0;
 
-        if (getTipo().equalsIgnoreCase("Conta Corrente")) {
+        if (this.getTipo().equalsIgnoreCase("cc")) {
             valorMensalidade = 12;
-        } else if (getTipo().equalsIgnoreCase("Conta Poupança")) {
+        } else if (this.getTipo().equalsIgnoreCase("cp")) {
             valorMensalidade = 20;
         }
 
-        if (isStatus() == false) {
+        if (!isStatus()) {
             System.out.println("Você não tem uma conta ativa!");
         } else {
-            if (getSaldo() < 0) {
-                setSaldo(getSaldo() - valorMensalidade);
-                System.out.println("Você pagou a mensalidade de R$" + valorMensalidade + " e ficou com R$" + getSaldo() + " de Devedor pro Banco!");
+            if (this.getSaldo() < 0) {
+                this.setSaldo(this.getSaldo() - valorMensalidade);
+                System.out.println("Você pagou a mensalidade de R$" + valorMensalidade + " e ficou com R$" + this.getSaldo() + " de Devedor pro Banco!");
             } else {
-                setSaldo(getSaldo() - valorMensalidade);
+                this.setSaldo(this.getSaldo() - valorMensalidade);
                 System.out.println("Foi descontado R$" + valorMensalidade + " de sua conta para pagar a Mensalidade!");
             }
         }
+        mostrarSaldo();
         pularLinha();
     }
 
     private void mostrarSaldo() {
         System.out.println("Seu saldo atual é de R$" + getSaldo());
+    }
+
+
+    // Métodos Especiais
+
+    public ContaBanco() {
+        this.setSaldo(0);
+        this.setStatus(false);
     }
 
     public int getNumConta() {
@@ -128,13 +137,6 @@ public class ContaBanco {
     }
 
     public void setTipo(String tipo) {
-
-        if (tipo.equalsIgnoreCase("cp")) {
-            tipo = "Conta Poupança";
-        } else if (tipo.equalsIgnoreCase("cc")) {
-            tipo = "Conta Corrente";
-        }
-
         this.tipo = tipo;
     }
 
